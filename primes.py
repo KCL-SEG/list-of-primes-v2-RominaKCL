65e6a65ff6eba35de77cfdbf59c07661aac467a8
Romina <k22034001@kcl.ac.uk>
2023-10-02 00:19:05 +0100

    The solution to exercise 4

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if(number_of_primes < 1):
+        raise ValueError(f"{number_of_primes} should be a number bigger than zero!")
+    list = [2]
+    i=3
+    while number_of_primes-len(list) > 0:
+        j=2
+        count=True
+        while j * j <= i:
+            if(i%j==0):
+                count=False
+                break
+            j+=1
+        if(count):
+            list.append(i)
+        i+=2
     return list
